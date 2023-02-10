dc045814d12c58275ec8ce82e5f9cac700ceee08
Lea Verou <lea@verou.me>
2023-02-10 11:43:38 -0500

    Simplify exercise

--- index.js
+++ index.js
@@ -14,26 +14,31 @@ let app = createApp({
 	methods: {
 		addItem() {
 			this.tasks.push({});
-			this.tasks.at(-1).active = true;
+
+			this.setActive();
 		},
 
 		deleteItem(i) {
 			this.tasks.splice(i, 1);
-
-			if (this.tasks[i-1]) {
-				this.tasks[i-1].active = true;
-			}
-		},
-
-		deleteItemIfEmpty(i) {
-			if (this.tasks[i].title === "") {
-				this.deleteItem(i);
-			}
+			this.setActive(i - 1);
 		},
 
 		clearCompleted () {
 			this.tasks = this.tasks.filter(task => !task.done);
-		}
+		},
+
+		setActive(i) {
+			for (let task of this.tasks) {
+				task.active = false;
+			}
+
+			if (i >= 0 && this.tasks[i]) {
+				this.tasks[i].active = true;
+			}
+			else if (this.tasks.length > 0) {
+				this.tasks.at(-1).active = true;
+			}
+		},
 	}
 }, "#app");
 
