773e5bda88f2c27c89be030bc3716f2d2e542cf0
Lea Verou <lea@verou.me>
2023-02-09 21:19:22 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import { createApp, Vue } from "https://mavue.mavo.io/mavue.js";
+
+let app = createApp({
+	data: {
+		tasks: [{}]
+	},
+
+	methods: {
+		addItem() {
+			this.tasks.push({});
+			this.tasks.at(-1).active = true;
+		},
+
+		deleteItem(i) {
+			this.tasks.splice(i, 1);
+			this.tasks[i-1].active = true;
+		},
+
+		clearCompleted () {
+			this.tasks = this.tasks.filter(task => !task.done);
+		}
+	}
+}, "#app");
